61af1cf10c23cc0be5219c60030a0952ce86d34a
Mariano Masuelli <marianomasuelli@hotmail.com>
2023-11-04 23:38:15 -0300

    Repair

--- React/src/Componentes/Error.jsx
+++ React/src/Componentes/Error.jsx
@@ -1,7 +1,5 @@
-import img from '../assets/404.jpg';
 export default function Error() {
   return (
-    <div><img src={img} alt="Error 404" /></div>
-    
+<h1 className="NotFound">No se pudo encontrar la página</h1>    
   );
 }
